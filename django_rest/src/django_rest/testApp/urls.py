0e6c91b98bd5fe73e4e9613a899093234a7223ac
neomn110 <neomn110@gmail.com>
2023-08-15 18:45:30 +0330

    fix: correct typos

--- django_rest/src/django_rest/testApp/urls.py
+++ django_rest/src/django_rest/testApp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 
-urlPatterns = [
+urlpatterns = [
     path('', views.getResponse),
 ]
--- django_rest/src/django_rest/testApp/views.py
+++ django_rest/src/django_rest/testApp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from rest_framework.response import Response
-from reset_framework.decorators import api_view
+from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def getResponse(request):
