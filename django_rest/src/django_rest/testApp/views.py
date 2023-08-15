233c077c300458552571e2fb8c7e95040fac3d8b
neomn110 <neomn110@gmail.com>
2023-08-15 14:40:43 +0330

    feat: add a test response

--- django_rest/src/django_rest/testApp/views.py
+++ django_rest/src/django_rest/testApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.response import Response
+from reset_framework.decorators import api_view
+
+@api_view(['GET'])
+def getResponse(request):
+    message = 'welcome to django reset framework'
+    return Response(message)
+    
